5c39ce74ca06ffe44cfbd110cfb3c6dbd1c9dd72
sk <kangsoonwoo2@gmail.com>
2025-02-13 12:24:41 -0400

    Initial commit - Imperial March TI-Nspire

---
+++ imperial-march.py
@@ -0,0 +1,44 @@
+
+# code only valid on TI Nspire calculator.
+
+from ti_hub import*
+
+sound.note("A4", 0.5)
+sleep(0.5)
+sound.note("A4", 0.5)
+sleep(0.5)
+sound.note("A4", 0.5)
+sleep(0.5)
+sound.note("F4", 0.35)
+sleep(0.35)
+sound.note("C5", 0.15)
+sleep(0.15)
+
+sound.note("A4", 0.5)
+sleep(0.5)
+sound.note("F4", 0.35)
+sleep(0.35)
+sound.note("C5", 0.15)
+sleep(0.15)
+sound.note("A4", 1)
+sleep(1)
+
+sound.note("E5", 0.5)
+sleep(0.5)
+sound.note("E5", 0.5)
+sleep(0.5)
+sound.note("E5", 0.5)
+sleep(0.5)
+sound.note("F5", 0.35)
+sleep(0.35)
+sound.note("C5", 0.15)
+sleep(0.15)
+
+sound.note("A4", 0.5)
+sleep(0.5)
+sound.note("F4", 0.35)
+sleep(0.35)
+sound.note("C5", 0.15)
+sleep(0.15)
+sound.note("A4", 1)
+sleep(1)
